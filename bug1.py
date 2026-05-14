c5d8203e6272e67572c9c435b0f9889648d3d5ff
esedovam92-collab <esedova.m92@gmail.com>
2026-05-14 12:11:32 +0400

    fix

--- bug1.py
+++
@@ -1,7 +0,0 @@
-def divide_numbers(a, b):
-    return a / b
-
-numbers = [20, 10, 0]
-
-for i in range(len(numbers)):
-    print(divide_numbers(numbers[i], numbers[i + 1]))
--- bug2.js
+++
@@ -1,9 +0,0 @@
-function printItems(items) {
-    for (let i = 0; i <= items.length; i++) {
-        console.log(items[i].toUpperCase());
-    }
-}
-
-const fruits = ["apple", "banana", "orange"];
-
-printItems(fruits);
